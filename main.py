73cb0f57072552b0cf70913c70617b03a0c26a7c
Андрей Шим <Andrei@MacBook-Air-Oksana.local>
2023-03-31 12:16:56 +0300

    last commit

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+@app.route('/')
+def main():
+    return 'Hello world!'
+
+
+@app.route('/index')
+def index():
+    return 'It is my first project'
+
+@app.route('/index/<x>/<y>')
+def indexf(x,y):
+    return f'Сумма {int(x) + int(y)}'
+
+if __name__ == '__main__':
+    app.run()
